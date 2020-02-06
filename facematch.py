ad8374c8567a97d70013e5e3f40981804ea7c54c
Patrick Assaf <patrickassaf96@gmail.com>
2020-02-05 19:06:34 -0800

    Implement module to pull faces from group photos and save them as separate images

--- facematch.py
+++ facematch.py
@@ -6,8 +6,6 @@ gates_face_encoding = face_recognition.face_encodings(gates_image)[0]
 unknown_image = face_recognition.load_image_file('./img/unknown/gates-2.jpg')
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-
-
 # Compare faces
 results = face_recognition.compare_faces([gates_face_encoding], unknown_face_encoding)
 
